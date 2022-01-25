89815955f9cf212ac81caf10c9791aa52a321bc4
jeffthorne <jthorne@u.washington.edu>
2022-01-25 08:48:48 -0800

    error handling

--- README.md
+++ README.md
@@ -7,12 +7,13 @@ Current Status: Experimental<br/><br/>
 ## Example Usage
 
     lwp --registries test.jfrog.io:443[username:${PASSWORD}],test2.jfrog.io:443[username:${ENCRYPTED_PASSWORD}]  \ 
-        --repo-keys default-docker-local --proxy-scanner-address http://192.168.1.41:9080 --num-images 3
+        --repo-keys default-docker-local --proxy-scanner-address http://192.168.1.41:9080 --num-images 3 --scans-hour 1500
 
 <br/><br/>
 lwp -h
 
 
 <br/><br/>
+--num-images is the number of images per image repository by created date<br/><br/>
 For detailed output set env variable RUST_LOG=info
  
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 extern crate serde;
 
 use std::borrow::Borrow;
-use clap::{AppSettings, Parser};
+use clap::{Parser};
 use std::collections::HashMap;
-use serde::{Deserialize, Serialize};
-use serde_json::{Result, Value, json};
+use serde_json::{Value, json};
 use std::cell::RefCell;
 use chrono::{Utc, DateTime, Local};
 use std::cmp::Reverse;
-use std::env;
 use std::io::Write;
+use futures::future::join_all;
+use std::sync::Arc;
+use std::sync::Mutex;
+use tokio::time::{sleep, Duration};
 
 
 /// A command line utility for interfacing with lwproxy in scheduling container
@@ -25,6 +27,8 @@ struct Opts {
     repo_keys: Vec<String>,
     #[clap(short, long, default_value = "1")]
     num_images: usize,
+    #[clap(short, long, default_value = "1500")]
+    scans_hour: usize,
     ///Full URL must be specified such as http://192.168.1.41:9080
     #[clap(short='l', long)]
     proxy_scanner_address: String,
@@ -47,17 +51,17 @@ pub struct Endpoint{
 }
 
 
-fn get_tags(endpoint: &Endpoint, repo_key: String, image_repository: String, opts: &Opts)-> RefCell<Vec<(String, DateTime<Utc>)>> {
-    let client = reqwest::blocking::Client::new();
+async fn get_tags(endpoint: &Endpoint, repo_key: String, image_repository: String)-> RefCell<Vec<(String, DateTime<Utc>)>> {
+    let client = reqwest::Client::new();
     let url = format!("https://{}/artifactory/api/docker/{}/v2/{}/tags/list", endpoint.name, repo_key.replace("\"", ""), image_repository.replace("\"", ""));
-    let resp = client.get(url).basic_auth(&endpoint.username, Some(&endpoint.password)).send().unwrap().json::<serde_json::Value>().unwrap();
-    let mut tags: RefCell<Vec<(String, DateTime<Utc>)>> = RefCell::new(Vec::new());
+    let resp = client.get(url).basic_auth(&endpoint.username, Some(&endpoint.password)).send().await.unwrap().json::<serde_json::Value>().await.unwrap();
+    let tags: RefCell<Vec<(String, DateTime<Utc>)>> = RefCell::new(Vec::new());
 
 
 
     for tag in resp.as_object().unwrap()["tags"].as_array().unwrap(){
         let url = format!("https://{}/artifactory/api/docker/{}/v2/{}/manifests/{}", endpoint.name, repo_key.replace("\"", ""), image_repository.replace("\"", ""), tag.to_string().replace("\"", ""));
-        let resp = client.get(url).basic_auth(&endpoint.username, Some(&endpoint.password)).send().unwrap().json::<serde_json::Value>().unwrap();
+        let resp = client.get(url).basic_auth(&endpoint.username, Some(&endpoint.password)).send().await.unwrap().json::<serde_json::Value>().await.unwrap();
 
         let history = resp.as_object().unwrap()["history"].as_array().unwrap().get(0).unwrap();
         let v:Value = serde_json::from_str(history.as_object().unwrap().get("v1Compatibility").unwrap().as_str().unwrap()).unwrap();
@@ -70,23 +74,24 @@ fn get_tags(endpoint: &Endpoint, repo_key: String, image_repository: String, opt
     return tags
 }
 
-fn get_images(endpoints: &Vec<Endpoint>, opts: &Opts){
+async fn get_images(endpoints: &Vec<Endpoint>){
 
-    let client = reqwest::blocking::Client::new();
+    let client = reqwest::Client::new();
 
 
     for endpoint in endpoints {
         let mut repo_keys = endpoint.repo_keys.borrow_mut();
 
+
         for (repo_key, _) in repo_keys.clone().iter() {
             let url = format!("https://{}/artifactory/api/docker/{}/v2/_catalog", endpoint.name, repo_key.replace("\"", ""));
-            let resp = client.get(url).basic_auth(&endpoint.username, Some(&endpoint.password)).send().unwrap().json::<serde_json::Value>().unwrap();
-            println!("RESP: {}", resp);
+            let response = client.get(url).basic_auth(&endpoint.username, Some(&endpoint.password)).send().await.expect(format!("ERROR: connecting to {:?}", &endpoint.name).as_str());
+            let resp = response.json::<serde_json::Value>().await.expect(format!("ERROR: connecting to {:?}", &endpoint.name).as_str());
             let repos = resp.as_object().unwrap()["repositories"].as_array().unwrap();
 
             let mut repositories: Vec<Repository> = Vec::new();
             for repo in repos {
-                let tags = get_tags(&endpoint, repo_key.clone(), repo.to_string(), opts);
+                let tags = get_tags(&endpoint, repo_key.clone(), repo.to_string()).await;
                 let repository = Repository{name: repo.to_string().replace("\"", ""), tags: tags};
                 repositories.push(repository);
             }
@@ -98,17 +103,18 @@ fn get_images(endpoints: &Vec<Endpoint>, opts: &Opts){
 
 }
 
-fn get_repo_keys(endpoints: &Vec<Endpoint>, opts: &Opts){
+async fn get_repo_keys(endpoints: &Vec<Endpoint>){
 
-    let client = reqwest::blocking::Client::new();
+    let client = reqwest::Client::new();
 
     for endpoint in endpoints {
         if endpoint.repo_keys.borrow().len() == 0{
 
             let url = format!("https://{}/artifactory/api/repositories", endpoint.name);
-            let repo_keys = client.get(url).basic_auth(&endpoint.username, Some(&endpoint.password)).send().unwrap().json::<serde_json::Value>().unwrap();
+            let resp = client.get(url).basic_auth(&endpoint.username, Some(&endpoint.password)).send().await.unwrap();
+            let repo_keys = resp.json::<serde_json::Value>().await.unwrap();
 
-
+            println!("REPO_KEYS: {}", repo_keys);
             for repo_key in repo_keys.as_array().unwrap(){
                 for (key, value) in repo_key.as_object().unwrap() {
                     if key == "packageType" && value.as_str().unwrap() == "Docker"{
@@ -131,8 +137,9 @@ fn set_up_scan_jobs(endpoints: &Vec<Endpoint>, options: &Opts) -> Vec<serde_json
    for endpoint in endpoints.iter(){
        for (name, repos) in endpoint.borrow().repo_keys.borrow().clone().iter(){
            for repo in repos {
-               let num_tags = if options.num_images < repo.tags.borrow().len() { options.num_images  } else { repo.tags.borrow().len()  };
-               for tag in repo.tags.borrow()[..num_tags].iter(){
+               let num_images = if options.num_images < repo.tags.borrow().len() { options.num_images  } else { repo.tags.borrow().len()  };
+               //println!("NUM IMAGES: {}:{:?}", num_images, endpoint.repo_keys);
+               for tag in repo.tags.borrow()[..num_images].iter(){
                    let registry = format!("{}/artifactory/api/docker/{}", endpoint.name, name);
                    let obj = json!({"registry": registry, "image_name": repo.name, "tag": tag.0});
                    scan_jobs.push(obj);
@@ -144,75 +151,151 @@ fn set_up_scan_jobs(endpoints: &Vec<Endpoint>, options: &Opts) -> Vec<serde_json
     return scan_jobs
 }
 
-fn scan_images(scan_jobs: Vec<serde_json::Value>, opts: &Opts){
-    let client = reqwest::blocking::Client::new();
+
+
+async fn scan_image(scan_job: &serde_json::Value, opts: &Opts, summary: &Arc<Mutex<Vec<serde_json::Value>>>){
+    log::debug!("In SCAN IMAGES");
+    let client = reqwest::Client::new();
     let url = format!("{}/v1/scan", opts.proxy_scanner_address);
+    let registry = &scan_job.as_object().unwrap().get("registry").unwrap().to_string().replace("\"", "");
+    let image_name = &scan_job.as_object().unwrap().get("image_name").unwrap().to_string().replace("\"", "");
+    let image_tag = &scan_job.as_object().unwrap().get("tag").unwrap().to_string().replace("\"", "");
 
-    for scan_job in scan_jobs.iter(){
-        log::info!("SCHEDULING SCAN FOR -> REGISTRY: {:#?} IMAGE: {:#?}:{:#?}",
-            &scan_job.as_object().unwrap().get("registry").unwrap().to_string().replace("\"", ""),
-            &scan_job.as_object().unwrap().get("image_name").unwrap().to_string().replace("\"", ""),
-            &scan_job.as_object().unwrap().get("tag").unwrap().to_string().replace("\"", "")
-        );
 
-        let resp = match client.post(&url).json(&scan_job).send() {
+        match client.post(&url).json(&scan_job).send().await {
             Ok(response) => {
-                log::info!("SCAN RESULT FOR -> REGISTRY: {:#?} IMAGE: {:#?}:{:#?}. Status: {:?}. Result: {:#?}",
-                &scan_job.as_object().unwrap().get("registry").unwrap().to_string().replace("\"", ""),
-                &scan_job.as_object().unwrap().get("image_name").unwrap().to_string().replace("\"", ""),
-                &scan_job.as_object().unwrap().get("tag").unwrap().to_string().replace("\"", ""),
-                response.status(),
-                response.json::<serde_json::Value>().unwrap().as_object().unwrap().get("ok").unwrap().to_string()
-                );
+                match response.json::<serde_json::Value>().await{
+                    Ok(resp) => {
+                        let status_code = resp.as_object().unwrap()["status_code"].as_u64().unwrap();
+                        if status_code == 200 as u64 {
+                            println!("[SCAN REQUEST] REGISTRY: {:#?} IMAGE: {:#?}:{:#?} [RESULT] SUCCESS -> {}", registry, image_name, image_tag, resp);
+                        }else{
+                            println!("[SCAN REQUEST] REGISTRY: {:#?} IMAGE: {:#?}:{:#?} [RESULT] SUCCESS -> {}", registry, image_name, image_tag, resp);
+                            let obj = json!({"registry": registry, "image_name": image_name, "tag": image_tag, "error": resp});
+                            summary.lock().unwrap().push(obj);
+                        }
+
+                    },
+                    Err(e) => {
+                        println!("BIG ERROR 1");
+                        println!("[SCAN REQUEST] REGISTRY: {:#?} IMAGE: {:#?}:{:#?} [RESULT] FAILURE -> {}", registry, image_name, image_tag, e);
+                        let obj = json!({"registry": registry, "image_name": image_name, "tag": image_tag, "error": e.to_string()});
+                        summary.lock().unwrap().push(obj);
+                    }
+                }
             },
-            Err(e) => println!("ERROR: {:?}", e)
+            Err(e) => {
+                println!("BIG ERROR 2");
+                println!("[SCAN REQUEST] -> REGISTRY: {:#?} IMAGE: {:#?}:{:#?} [RESULT] FAILURE -> {}", registry, image_name, image_tag, e);
+                //let obj = json!({"registry": registry, "image_name": image_name, "tag": image_tag, "error": e.to_string()});
+                //summary.lock().unwrap().push(obj);
+                panic!("{:?}", e);
+            }
         };
 
-    }
+
 }
 
 pub fn setup_logging(){
 
-    env_logger::builder()
-        .format(|buf, record| {
-            writeln!(buf,
-                     "{} [{}] - {}",
-                     Local::now().format("%Y-%m-%dT%H:%M:%S"),
-                     record.level(),
-                     record.args()
-            )
+    env_logger::builder().format(|buf, record| {
+            writeln!(buf, "{} [{}] - {}", Local::now().format("%Y-%m-%dT%H:%M:%S"),record.level(), record.args())
         })
         .init();
 }
 
-fn main() {
+fn print_registries(registries: &Vec<&str>){
+    let mut temp_registries: Vec<&str> = vec![];
+
+    for registry in registries{
+        let chunks: Vec<&str> = registry.split('[').collect();
+        temp_registries.push(chunks[0]);
+    }
+    println!("REGISTRIES: {:?}", temp_registries);
+
+}
+
+fn print_summary(scan_jobs: &Vec<serde_json::Value>, summary: &Arc<Mutex<Vec<serde_json::Value>>>){
+    println!("LENGTH OF SCAN JOBS: {}", scan_jobs.len());
+    println!("SUCCESSFUL SCAN JOBS: {}", scan_jobs.len() - summary.lock().unwrap().len());
+    println!("FAILED SCANS:\n{:?}", summary.lock().unwrap());
+}
+
+#[tokio::main]
+async fn main() {
     let opts = Opts::parse();
     let options: Opts = opts.clone();
+    let summary: Arc<Mutex<Vec<serde_json::Value>>> = Arc::new(Mutex::new(vec![]));
     setup_logging();
 
     let registries: Vec<&str> = options.registries.split(',').collect();
-    println!("REGISTRIES: {:?}", registries);
+    print_registries(&registries);
 
-    let mut endpoints: Vec<Endpoint> = registries.into_iter().map(|x| {
+    let endpoints: Vec<Endpoint> = registries.into_iter().map(|x| {
 
         let registry_info: Vec<&str> = x.split(&['[',']', ':'][..]).collect();
-        let mut repo_keys: RefCell<HashMap<String, Vec<Repository>>> = RefCell::new(HashMap::new());
+        let repo_keys: RefCell<HashMap<String, Vec<Repository>>> = RefCell::new(HashMap::new());
         for repo_key in opts.repo_keys.clone(){
             repo_keys.borrow_mut().insert(repo_key, Vec::new());
         }
 
-        let mut endpoint = Endpoint{name: format!("{}:{}", registry_info[0], registry_info[1]), repo_keys: repo_keys, username:registry_info[2].to_string(),
-                                    password:registry_info[3].to_string() };
-        println!("ENDPOINT: {:?}", endpoint);
+        let endpoint = Endpoint{name: format!("{}:{}", registry_info[0], registry_info[1]), repo_keys: repo_keys, username:registry_info[2].to_string(), password:registry_info[3].to_string() };
+        log::info!("ENDPOINT: {:?}", endpoint);
         return endpoint
     }).rev().collect();
 
-    get_repo_keys(&endpoints, &options);
-    get_images(&endpoints, &options);
+
+    get_repo_keys(&endpoints).await;
+    get_images(&endpoints).await;
     let scan_jobs = set_up_scan_jobs(&endpoints, &options);
-    println!("SCAN JOBS: {:?}", scan_jobs);
-    println!("LENGTH OF SCAN JOBS: {}", scan_jobs.len());
-    //scan_images(scan_jobs[..1].to_vec(), &options);
+
+
+
+    let mut scan_count = 0;
+    let scans_per_hour = options.scans_hour;
+    let done: Arc<Mutex<bool>> = Arc::new(Mutex::new(false));
+    let mut scans_per_hour = scans_per_hour;
+
+    println!("TOTAL SCAN JOBS FOUND: {:?}", scan_jobs.len());
+    if scan_jobs.len() < scans_per_hour{
+        scans_per_hour = scan_jobs.len()
+    }
+
+    loop {
+        if scans_per_hour > scan_jobs.len(){
+
+            println!("SCANNING IMAGES {} - {} :: {}", scan_count + 1, scan_count + scans_per_hour, Utc::now());
+            let futures = scan_jobs.iter().map(|scan_job| scan_image(scan_job, &options, &summary));
+            join_all(futures).await;
+            *done.lock().unwrap() = true;
+            print_summary(&scan_jobs, &summary);
+            break;
+
+
+        }else{
+            if scan_count + scans_per_hour >= scan_jobs.len(){
+                println!("SCANNING IMAGES {} - {}  :: {}", scan_count + 1, scan_jobs.len(), Utc::now());
+                let futures = scan_jobs[scan_count..scan_jobs.len()].iter().map(|scan_job| scan_image(scan_job, &options, &summary));
+                join_all(futures).await;
+                *done.lock().unwrap() = true;
+                print_summary(&scan_jobs, &summary);
+                break;
+
+
+            }else{
+                println!("SCANNING IMAGES {} - {} :: {}", scan_count + 1, scan_count + scans_per_hour, Utc::now());
+                let futures = scan_jobs[scan_count..scan_count + scans_per_hour].iter().map(|scan_job| scan_image(scan_job, &options, &summary));
+                join_all(futures).await;
+                scan_count = scan_count + scans_per_hour;
+            }
+        }
+
+        if *done.lock().unwrap(){
+            break;
+        }
+
+       sleep(Duration::from_millis(3600000)).await;
+    }
 
 
 
